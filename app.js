a4c52bc51ec124e0f7008d9e2d5ce862af522742
git_Nikos <nikosoikonomou@gmail.com>
2022-11-18 20:55:18 +0200

    Minor Css Upadate

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const blueBox = document.getElementById('blueBox');
 const containers = document.querySelectorAll('.container');
 const dragableItems = document.querySelectorAll('.draggable');
 
-const smallBoxesValueCheck = () => {
+const smallBoxesTxtContentCheck = () => {
   let redDivsTxtContent = [];
   let blueDivsTxtContent = [];
   redBox.querySelectorAll('div').forEach(box => redDivsTxtContent.push(box.textContent));
@@ -14,17 +14,14 @@ const smallBoxesValueCheck = () => {
   for (let i = 0; i < redDivsTxtContent.length; i++) {
     if (isNaN(redDivsTxtContent[i])) {
       tempRedArray.push(redDivsTxtContent[i])
-      console.log(tempRedArray);
     } else if (isNaN(blueDivsTxtContent[i])) {
       tempBlueArray.push(blueDivsTxtContent[i])
-      console.log(tempBlueArray);
     }
     if (tempRedArray.length == redDivsTxtContent.length && blueDivsTxtContent.length == 8 ||
       tempBlueArray.length == blueDivsTxtContent.length && redDivsTxtContent.length == 8) {
       alert('Well Done!!\nYou have successfully match the boxes!');
     }
   }
-
 }
 
 // -----   Small boxes inside the 2 Big boxes
@@ -37,7 +34,7 @@ dragableItems.forEach(ele => {
 dragableItems.forEach(ele => {
   ele.addEventListener('dragend', () => {
     ele.classList.remove('dragging');
-    smallBoxesValueCheck();
+    smallBoxesTxtContentCheck();
   });
 });
 
